39b3cac61bbc823f14aa5339f2de114734a07b6c
nickmwangemi <nickmwangemi@gmail.com>
2020-10-20 22:05:42 +0300

    Implemented product listing on the home screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+
+
 
 const App = () => {
   return (
@@ -9,8 +12,7 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>Welcome to Usafi Personal Care</h1>
-
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
