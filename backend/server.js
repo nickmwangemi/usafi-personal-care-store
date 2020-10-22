33b018508b3815b463720f63cfa2d9c2cc6e8545
nickmwangemi <nickmwangemi@gmail.com>
2020-10-22 23:38:55 +0300

    Implemented custom error handling

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 import colors from 'colors'
 
@@ -22,6 +23,12 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/v1/products', productRoutes)
+
+// Handles invalid routes
+app.use(notFound)
+
+// Handles errors in request
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 app.listen(
