f694ad36f4f4003260a5e0d82d0b194071622f33
nickmwangemi <nickmwangemi@gmail.com>
2020-10-29 16:38:18 +0300

    Include Message component in HomeScreen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -4,6 +4,7 @@ import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
 import { listProducts } from '../actions/productActions'
 import Loader from '../components/Loader'
+import Message from '../components/Message'
 
 const HomeScreen = () => {
 	const dispatch = useDispatch()
@@ -21,7 +22,7 @@ const HomeScreen = () => {
 			{loading ? (
 				<Loader />
 			) : error ? (
-				<h3>{error}</h3>
+				<Message variant='danger'>{error}</Message>
 			) : (
 				<Row>
 					{products.map((product) => (
