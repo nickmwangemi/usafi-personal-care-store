c708f937dd79d27f0ec341fa226884458241cdf2
nickmwangemi <nickmwangemi@gmail.com>
2020-11-12 19:25:05 +0300

    Update products

--- backend/data/products.js
+++ backend/data/products.js
@@ -3,7 +3,7 @@ const products = [
 		name: 'Old Spice Aqua Reef',
 		image: '/images/old-spice-aqua-reef-main.jpg',
 		description:
-			'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+			"Aqua Reef is a lime and cypress scent reminiscent of drifting around in an inner tube. Old Spice Aqua Reef Men's Deodorant overpowers stink with good smellingness.",
 		brand: 'Old Spice',
 		category: 'Deodorant',
 		price: 89.99,
@@ -15,7 +15,7 @@ const products = [
 		name: 'Old Spice Fiji',
 		image: '/images/old-spice-fiji-main.jpg',
 		description:
-			'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+			'Fiji with Palm Tree Antiperspirant glides on clear & reduces armpit sweat, keeping pits drier than with a deodorant alone.Fiji with Palm Tree smells like an island vacation: fresh, breezy and drenched in piña coladas--oh yeah, and lavender and coconut, too.',
 		brand: 'Old Spice',
 		category: 'Deodorant',
 		price: 89.99,
@@ -27,7 +27,7 @@ const products = [
 		name: 'Old Spice Pure Sport',
 		image: '/images/old-spice-pure-sport.jpg',
 		description:
-			'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+			"Old Spice Men's Deodorant reduces underarm odor for 24 hours. Turn the base to raise the deodorant and wipe armpits for long-lasting freshness. I'm talking about the sophisticated scent of a man who uses Old Spice. Pure Sport is the crisp, clean scent of achievement. Old Spice Men's Deodorant overpowers even the burliest stink with overwhelming good-smellingness. So get yourself some High Endurance Collection and prepare for success, because it will find you no matter what.",
 		brand: 'Old Spice',
 		category: 'Deodorant',
 		price: 89.99,
@@ -39,7 +39,7 @@ const products = [
 		name: 'Old Spice Swagger',
 		image: '/images/old-spice-swagger-aluminum-free.jpg',
 		description:
-			'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+			'Swagger Antiperspirant and Deodorant goes on invisible and keeps you feeling dry and odor-free all day. Swagger is the scent of confidence, which happens to smell like lime and cedarwood. Part of our Red Zone Collection, Swagger transforms unfresh men into legends of confidence. Antiperspirant gives you odor fighting protection like deodorant, but fights sweat too',
 		brand: 'Old Spice',
 		category: 'Deodorant',
 		price: 89.99,
@@ -51,7 +51,7 @@ const products = [
 		name: 'Old Spice Tundra',
 		image: '/images/old-spice-tundra-main.jpg',
 		description:
-			'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+			"Freshness can be an elusive thing. Sometimes it takes rolling up your sleeves and lacing up your boots to find the real roots of freshness. Thankfully, we've done the adventuring for you so that you can smell like you've been exploring even if you haven't had time to explore that day. Tundra With Mint smells as clean as the driven snow and minty like a mint that makes mints instead of money.",
 		brand: 'Old Spice',
 		category: 'Deodorant',
 		price: 89.99,
@@ -63,7 +63,7 @@ const products = [
 		name: 'Old Spice Volcano',
 		image: '/images/old-spice-volcano-main.jpg',
 		description:
-			'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+			'Old Spice Volcano with Charcoal Antiperspirant and Deodorant goes on invisible, reduces sweat and keeps those pits feeling dry. Volcano with Charcoal deodorant and antiperspirant smells like citrus, amber and the bravery it takes to approach a mountain of lava and think "I got this". Our Fresher Collection is forged with real ingredients and all the freshness of nature- just like the volcanoes where the freshest new rocks are born.',
 		brand: 'Old Spice',
 		category: 'Deodorant',
 		price: 89.99,
@@ -75,7 +75,7 @@ const products = [
 		name: 'Secret Summer Berry',
 		image: '/images/secret-summer-berry.jpg',
 		description:
-			'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+			'Designed with scents that last, the Secret Fresh Collection has scents you love from our former Scent Expressions and Destinations collections. Secret Clear Gel Antiperspirant and Deodorant goes on clear, dries quickly, and helps stop underarm odor before it starts with 48-hour odor control and wetness protection. Discover unstoppable freshness with Secret Clear Gel.',
 		brand: 'Old Spice',
 		category: 'Deodorant',
 		price: 89.99,
@@ -87,7 +87,7 @@ const products = [
 		name: 'Old Spice Aqua Reef',
 		image: '/images/old-spice-aqua-reef-main.jpg',
 		description:
-			'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+			"Aqua Reef is a lime and cypress scent reminiscent of drifting around in an inner tube. Old Spice Aqua Reef Men's Deodorant overpowers stink with good smellingness.",
 		brand: 'Old Spice',
 		category: 'Deodorant',
 		price: 89.99,
