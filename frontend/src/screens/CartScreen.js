2fc19d8a3acc3d47f2ae4037279a94aaa59cd79d
nickmwangemi <nickmwangemi@gmail.com>
2020-11-12 07:57:45 +0300

    Implement Cart Screen elements

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -18,7 +18,110 @@ const CartScreen = ({ match, location, history }) => {
 		}
 	}, [dispatch, productId, quantity])
 
-	return <div>Cart</div>
+	const removeFromCartHandler = (id) => {
+		console.log('remove')
+	}
+
+	const checkoutHandler = () => {
+		history.push('/login?redirect=shipping')
+	}
+
+	return (
+		<Row>
+			<Col md={8}>
+				<h1>Shopping Cart</h1>
+				{cartItems.length === 0 ? (
+					<Message>
+						Your cart is empty
+						<Link to='/'> Go Back</Link>
+					</Message>
+				) : (
+					<ListGroup variant='flush'>
+						{cartItems.map((item) => (
+							<ListGroup.Item key={item.product}>
+								<Row>
+									<Col md={2}>
+										<Image
+											src={item.image}
+											alt={item.name}
+											fluid
+											rounded
+										></Image>
+									</Col>
+									<Col md={3}>
+										<Link to={`/product/${item.product}`}>{item.name}</Link>
+									</Col>
+									<Col md={2}>Ksh.&nbsp;{item.price}</Col>
+									<Col md={2}>
+										<Form.Control
+											as='select'
+											value={item.quantity}
+											onChange={(e) =>
+												dispatch(
+													addToCart(item.product, Number(e.target.value))
+												)
+											}
+										>
+											{[...Array(item.countInStock).keys()].map((x) => (
+												<option key={x + 1} value={x + 1}>
+													{x + 1}
+												</option>
+											))}
+										</Form.Control>
+									</Col>
+									<Col md={2}>
+										<Button
+											type='button'
+											variant='light'
+											onClick={() => removeFromCartHandler(item.product)}
+										>
+											<i className='fas fa-trash'></i>
+										</Button>
+									</Col>
+								</Row>
+							</ListGroup.Item>
+						))}
+					</ListGroup>
+				)}
+			</Col>
+			<Col md={4}>
+				<Card>
+					<ListGroup variant='flush'>
+						<ListGroup.Item>
+							<h2>
+								Subtotal (
+								{cartItems.reduce(
+									(accumulator, currentItem) =>
+										accumulator + currentItem.quantity,
+									0
+								)}
+								) items
+							</h2>
+							Ksh. &nbsp;
+							{cartItems
+								.reduce(
+									(accumulator, currentItem) =>
+										accumulator + currentItem.quantity * currentItem.price,
+									0
+								)
+								.toFixed(2)}
+						</ListGroup.Item>
+						<ListGroup.Item>
+							<Button
+								type='button'
+								className='btn-block'
+								disabled={cartItems.length === 0}
+								onClick={checkoutHandler}
+							>
+								Proceed to Checkout
+							</Button>
+						</ListGroup.Item>
+					</ListGroup>
+				</Card>
+			</Col>
+			<Col md={8}></Col>
+		</Row>
+	)
 }
 
 export default CartScreen
