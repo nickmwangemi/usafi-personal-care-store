eb361b6d567a0901f7ac37853ac6c4690ff39133
nickmwangemi <nickmwangemi@gmail.com>
2020-11-12 16:51:14 +0300

    Update Mongo URI

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,19 +1,19 @@
-import mongoose from 'mongoose';
+import mongoose from 'mongoose'
 import colors from 'colors'
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI_LOCAL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true
-        })
+	try {
+		const conn = await mongoose.connect(process.env.MONGO_URI, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+		})
 
-        console.log(`\nMongoDB Connected: ${conn.connection.host} `.cyan.underline)
-    } catch (error) {
-        console.error(`\nError: ${error.message}`.red.underline.bold)
-        process.exit(1)
-    }
+		console.log(`\nMongoDB Connected: ${conn.connection.host} `.cyan.underline)
+	} catch (error) {
+		console.error(`\nError: ${error.message}`.red.underline.bold)
+		process.exit(1)
+	}
 }
 
 export default connectDB
